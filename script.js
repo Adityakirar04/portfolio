e4b415e12c0a6f96a16ac4ac631836d9695d8beb
Adityakirar04 <mr.kirar62@gmail.com>
2026-06-18 18:20:53 +0530

    new

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         try {
-            const response = await fetch("http://localhost:5000/contact", {
+            const response = await fetch(" https://portfolio-jphq.onrender.com/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
